fe7f7c286f03bc18598f262e47e8e67796e34474
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-05-24 12:24:23 +0200

    gateware/GpioTop.py: improve/cleanup

--- gateware/GpioTop.py
+++ gateware/GpioTop.py
@@ -1,69 +1,54 @@
 #!/usr/bin/env python3
 
 from migen import *
+
+from litex.gen import *
+
 from litex.soc.interconnect.csr import *
 
-
-class GpioTop(Module, AutoCSR):
+class GpioTop(LiteXModule):
     def __init__(self, platform, pads):
+        self.GPIO_DIR     = Signal(len(pads))
+        self.GPIO_OUT_VAL = Signal(len(pads))
+        self.GPIO_IN_VAL  = Signal(len(pads))
+
         # CSRs
-        self.control = CSRStorage(fields=[
-            CSRField("gpio_override", size = len(pads),  offset=0*len(pads), values=[
-            ("``0b0``", "GPIO normal operation"),
-            ("``0b1``", "GPIO control is overriden")
-            ], reset=0),
-            CSRField("gpio_override_dir", size=len(pads), offset=1*len(pads), values=[
-                ("``0b0``", "Output"),
-                ("``0b1``", "Input")
-            ], reset=0),
-            CSRField("gpio_override_val", size=len(pads), offset=2*len(pads), values=[
-                ("``0b0``", "Set output to logic High, dir must be set to output"),
-                ("``0b1``", "Set output to logic Low, dir must be set to output")
-            ], reset=0)
-        ])
+        self.gpio_override = CSRStorage(len(pads),
+            description="GPIO Mode: 0: normal operation, 1: control is overriden."
+        )
+        self.gpio_override_dir = CSRStorage(len(pads),
+            description="GPIO override direction: 0: Output, 1: Input."
+        )
+        self.gpio_override_val = CSRStorage(len(pads),
+            description="GPIO Logic level: 0: High, 1: Low. (Dir must be set to output)"
+        )
+        self.gpio_val = CSRStatus(size=len(pads), description="GPIO current value")
 
-        self.status   = CSRStatus(fields=[
-            CSRField("gpio_val", size=len(pads), offset=0*len(pads), values=[
-                ("GPIO current value")
-            ], reset=0)
-        ])
+        # # #
 
-        self.GPIO_DIR       = Signal(len(pads))
-        self.GPIO_OUT_VAL   = Signal(len(pads))
-        self.GPIO_IN_VAL    = Signal(len(pads))
+        # Signals.
         self.GPIO_I = Signal(len(pads))
         self.GPIO_O = Signal(len(pads))
         self.GPIO_T = Signal(len(pads))
 
-        ###
-
-        platform.add_source("./gateware/LimeIP_HDL/gpio_top/src/gpio_top.vhd")
-
-        # Create params
-        self.params_ios = dict()
-
-        # Assign generics
-        self.params_ios.update(
-            p_G_GPIO_WIDTH=len(pads)
-        )
-
-        # Assign ports
-        self.params_ios.update(
-            i_GPIO_DIR          =self.GPIO_DIR,
-            i_GPIO_OUT_VAL      =self.GPIO_OUT_VAL,
-            o_GPIO_IN_VAL       =self.GPIO_IN_VAL,
-            i_GPIO_OVERRIDE     =self.control.fields.gpio_override,
-            i_GPIO_OVERRIDE_DIR =self.control.fields.gpio_override_dir,
-            i_GPIO_OVERRIDE_VAL =self.control.fields.gpio_override_val,
-            i_GPIO_I            =self.GPIO_I,
-            o_GPIO_O            =self.GPIO_O,
-            o_GPIO_T            =self.GPIO_T
-        )
         # Assign GPIO current value to status register
-        self.comb += self.status.fields.gpio_val.eq(self.GPIO_IN_VAL)
+        self.comb += self.gpio_val.status.eq(self.GPIO_IN_VAL)
 
         # Create instance and assign params
-        self.specials += Instance("gpio_top", **self.params_ios)
+        self.specials += Instance("gpio_top",
+            # Assign generics
+            p_G_GPIO_WIDTH      = len(pads),
+            # Assign ports
+            i_GPIO_DIR          = self.GPIO_DIR,
+            i_GPIO_OUT_VAL      = self.GPIO_OUT_VAL,
+            o_GPIO_IN_VAL       = self.GPIO_IN_VAL,
+            i_GPIO_OVERRIDE     = self.gpio_override.storage,
+            i_GPIO_OVERRIDE_DIR = self.gpio_override_dir.storage,
+            i_GPIO_OVERRIDE_VAL = self.gpio_override_val.storage,
+            i_GPIO_I            = self.GPIO_I,
+            o_GPIO_O            = self.GPIO_O,
+            o_GPIO_T            = self.GPIO_T
+        )
 
         # Xilinx bidirectional buffer primitive
         for n in range(len(pads)):
@@ -73,3 +58,6 @@ class GpioTop(Module, AutoCSR):
                 i_I     = self.GPIO_O[n],
                 i_T     = self.GPIO_T[n]
             )
+
+        platform.add_source("./gateware/LimeIP_HDL/gpio_top/src/gpio_top.vhd")
+
