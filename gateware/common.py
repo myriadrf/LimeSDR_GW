30120fc17d111977828747c11fc5d244efb2125a
Florent Kermarrec <florent@enjoy-digital.fr>
2024-11-04 21:46:01 +0100

    gateware/common: Cleanup.

--- gateware/common.py
+++ gateware/common.py
@@ -9,18 +9,20 @@ from migen import *
 
 from litex.gen import *
 
+# FIFO Interface -----------------------------------------------------------------------------------
+
 class FIFOInterface(LiteXModule):
     def __init__(self, rdata_width, wdata_wdith, rdusedw_size=1, wrusedw_size=1):
-        self.rd          = Signal()
-        self.wr          = Signal()
-        self.rd_active   = Signal()
-        self.wr_active   = Signal()
-        self.rdata       = Signal(rdata_width)
-        self.wdata       = Signal(wdata_wdith)
-        self.empty       = Signal()
-        self.full        = Signal()
-        self.rdusedw     = Signal(rdusedw_size)
-        self.wrusedw     = Signal(wrusedw_size)
+        self.rd        = Signal()
+        self.wr        = Signal()
+        self.rd_active = Signal()
+        self.wr_active = Signal()
+        self.rdata     = Signal(rdata_width)
+        self.wdata     = Signal(wdata_wdith)
+        self.empty     = Signal()
+        self.full      = Signal()
+        self.rdusedw   = Signal(rdusedw_size)
+        self.wrusedw   = Signal(wrusedw_size)
 
     def connect(self, fifo_if):
         return [
@@ -36,62 +38,62 @@ class FIFOInterface(LiteXModule):
             self.wrusedw.eq(fifo_if.wrusedw),
         ]
 
-# Layout -------------------------------------------------------------------------------------------
+# Configuration Layouts ----------------------------------------------------------------------------
 
 from_fpgacfg_layout = [
-    ("phase_reg_sel", 16),       # std_logic_vector(15 downto 0)
-    ("clk_ind", 5),              # std_logic_vector(4 downto 0)
-    ("cnt_ind", 5),              # std_logic_vector(4 downto 0)
-    ("load_phase_reg", 1),       # std_logic
-    ("drct_clk_en", 16),         # std_logic_vector(15 downto 0)
-    ("ch_en", 16),               # std_logic_vector(15 downto 0)
-    ("smpl_width", 2),           # std_logic_vector(1 downto 0)
-    ("mode", 1),                 # std_logic
-    ("ddr_en", 1),               # std_logic
-    ("trxiq_pulse", 1),          # std_logic
-    ("mimo_int_en", 1),          # std_logic
-    ("synch_dis", 1),            # std_logic
-    ("synch_mode", 1),           # std_logic
-    ("smpl_nr_clr", 1),          # std_logic
-    ("txpct_loss_clr", 1),       # std_logic
-    ("rx_en", 1),                # std_logic
-    ("tx_en", 1),                # std_logic
-    ("rx_ptrn_en", 1),           # std_logic
-    ("tx_ptrn_en", 1),           # std_logic
-    ("tx_cnt_en", 1),            # std_logic
-    ("wfm_ch_en", 16),           # std_logic_vector(15 downto 0)
-    ("wfm_play", 1),             # std_logic
-    ("wfm_load", 1),             # std_logic
-    ("wfm_smpl_width", 2),       # std_logic_vector(1 downto 0)
-    ("SPI_SS", 16),              # std_logic_vector(15 downto 0)
-    ("LMS1_SS", 1),              # std_logic
-    ("LMS1_RESET", 1),           # std_logic
-    ("LMS1_CORE_LDO_EN", 1),     # std_logic
-    ("LMS1_TXNRX1", 1),          # std_logic
-    ("LMS1_TXNRX2", 1),          # std_logic
-    ("LMS1_TXEN", 1),            # std_logic
-    ("LMS1_RXEN", 1),            # std_logic
-    ("GPIO", 16),                # std_logic_vector(15 downto 0)
-    ("FPGA_LED1_CTRL", 3),       # std_logic_vector(2 downto 0)
-    ("FPGA_LED2_CTRL", 3),       # std_logic_vector(2 downto 0)
-    ("FX3_LED_CTRL", 3),         # std_logic_vector(2 downto 0)
-    ("CLK_ENA", 4),              # std_logic_vector(3 downto 0)
-    ("sync_pulse_period", 32),   # std_logic_vector(31 downto 0)
-    ("sync_size", 16),           # std_logic_vector(15 downto 0)
-    ("txant_pre", 16),           # std_logic_vector(15 downto 0)
-    ("txant_post", 16),          # std_logic_vector(15 downto 0)
+    ("phase_reg_sel",     16),
+    ("clk_ind",            5),
+    ("cnt_ind",            5),
+    ("load_phase_reg",     1),
+    ("drct_clk_en",       16),
+    ("ch_en",             16),
+    ("smpl_width",         2),
+    ("mode",               1),
+    ("ddr_en",             1),
+    ("trxiq_pulse",        1),
+    ("mimo_int_en",        1),
+    ("synch_dis",          1),
+    ("synch_mode",         1),
+    ("smpl_nr_clr",        1),
+    ("txpct_loss_clr",     1),
+    ("rx_en",              1),
+    ("tx_en",              1),
+    ("rx_ptrn_en",         1),
+    ("tx_ptrn_en",         1),
+    ("tx_cnt_en",          1),
+    ("wfm_ch_en",         16),
+    ("wfm_play",           1),
+    ("wfm_load",           1),
+    ("wfm_smpl_width",     2),
+    ("SPI_SS",            16),
+    ("LMS1_SS",            1),
+    ("LMS1_RESET",         1),
+    ("LMS1_CORE_LDO_EN",   1),
+    ("LMS1_TXNRX1",        1),
+    ("LMS1_TXNRX2",        1),
+    ("LMS1_TXEN",          1),
+    ("LMS1_RXEN",          1),
+    ("GPIO",              16),
+    ("FPGA_LED1_CTRL",     3),
+    ("FPGA_LED2_CTRL",     3),
+    ("FX3_LED_CTRL",       3),
+    ("CLK_ENA",            4),
+    ("sync_pulse_period", 32),
+    ("sync_size",         16),
+    ("txant_pre",         16),
+    ("txant_post",        16),
 ]
 
 to_tstcfg_from_rxtx_layout = [
-    ("DDR2_1_STATUS", 3),           # std_logic_vector(2 downto 0)
-    ("DDR2_1_pnf_per_bit", 32),     # std_logic_vector(31 downto 0)
+    ("DDR2_1_STATUS",       3),
+    ("DDR2_1_pnf_per_bit", 32),
 ]
 
 from_tstcfg_layout = [
-    ("TEST_EN", 6),      # std_logic_vector(5 downto 0);
-    ("TEST_FRC_ERR", 6), # std_logic_vector(5 downto 0);
-    ("TX_TST_I", 16),    # std_logic_vector(15 downto 0);
-    ("TX_TST_Q", 16),    # std_logic_vector(15 downto 0);
+    ("TEST_EN",      6),
+    ("TEST_FRC_ERR", 6),
+    ("TX_TST_I",    16),
+    ("TX_TST_Q",    16),
 ]
 
 def FromFPGACfg():
