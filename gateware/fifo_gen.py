1935a3c52beeb1ddb1c8deab2c146d040d075fd7
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-11-08 10:27:22 +0100

    fifo_gen.py: added option to uses SyncFIFOBuffered, to disable cdc. When no cdc a unique clk and rst are present instead of rd_clk, wr_clk, rd_rst, wr_rst

--- gateware/fifo_gen.py
+++ gateware/fifo_gen.py
@@ -31,6 +31,10 @@ from litex.soc.interconnect import stream
 
 def get_ios(input_width, output_width):
     return [
+        # Clk/Rst (only when not with-cdc).
+        ("clk",     0, Pins(1)),
+        ("rst",     0, Pins(1)),
+
         # Write IOs.
         ("wr_clk",  0, Pins(1)),
         ("wr_rst",  0, Pins(1)),
@@ -53,7 +57,14 @@ def get_ios(input_width, output_width):
 # FIFOGenerator ------------------------------------------------------------------------------------
 
 class FIFOGenerator(LiteXModule):
-    def __init__(self, platform, input_width=128, output_width=64, depth=32, reverse=False):
+    def __init__(self, platform,
+        input_width  = 128,
+        output_width = 64,
+        depth        = 32,
+        with_cdc     = False,
+        with_buffer  = False,
+        reverse      = False,
+        ):
         self.sink   = sink   = stream.Endpoint([("data",  input_width)])
         self.source = source = stream.Endpoint([("data", output_width)])
 
@@ -62,23 +73,31 @@ class FIFOGenerator(LiteXModule):
         platform.add_extension(get_ios(input_width, output_width))
         self.clock_domains.cd_wr = ClockDomain("wr")
         self.clock_domains.cd_rd = ClockDomain("rd")
-        self.comb += [
-            self.cd_wr.clk.eq(platform.request("wr_clk")),
-            self.cd_wr.rst.eq(platform.request("wr_rst")),
-            self.cd_rd.clk.eq(platform.request("rd_clk")),
-            self.cd_rd.rst.eq(platform.request("rd_rst")),
-        ]
+        if with_cdc:
+            self.comb += [
+                self.cd_wr.clk.eq(platform.request("wr_clk")),
+                self.cd_wr.rst.eq(platform.request("wr_rst")),
+                self.cd_rd.clk.eq(platform.request("rd_clk")),
+                self.cd_rd.rst.eq(platform.request("rd_rst")),
+            ]
+        else:
+            self.comb += [
+                self.cd_rd.clk.eq(platform.request("clk")),
+                self.cd_rd.rst.eq(platform.request("rst")),
+                self.cd_wr.clk.eq(self.cd_rd.clk),
+                self.cd_wr.rst.eq(self.cd_rd.rst),
+            ]
 
         # FIFO -------------------------------------------------------------------------------------
 
         # FIFO.
         # -----
-        self.fifo = ClockDomainsRenamer("wr")(stream.SyncFIFO([("data", input_width)], depth))
+        self.fifo = ClockDomainsRenamer("wr")(stream.SyncFIFO([("data", input_width)], depth, buffered=with_buffer))
 
         # CDC.
         # ----
         self.cdc = stream.ClockDomainCrossing([("data", input_width)],
-            cd_from         = "wr",
+            cd_from         = {True: "wr", False: "rd"}[with_cdc],
             cd_to           = "rd",
             with_common_rst = True,
         )
@@ -99,13 +118,16 @@ class FIFOGenerator(LiteXModule):
 
         # Interface --------------------------------------------------------------------------------
 
+        self.sync.rd += [
+            # Read.
+            source.ready.eq(platform.request("rd_en")),  # CHECKME: Latency?.
+        ]
         self.comb += [
             # Write.
             sink.valid.eq(platform.request("wr_en")),
             sink.data.eq(platform.request("wr_data")),
 
             # Read.
-            source.ready.eq(platform.request("rd_en")),  # CHECKME: Latency?.
             platform.request("rd_data").eq(source.data),
 
             # Status.
@@ -122,6 +144,8 @@ def main():
     parser.add_argument("--input-width",   default=128, help="Input data width  (default=128).")
     parser.add_argument("--output-width",  default=64,  help="Output data width (default=64).")
     parser.add_argument("--depth",         default=32,  help="Depth (default=32).")
+    parser.add_argument("--with-buffer",   action="store_true", help="Use SyncFIFOBuffered.")
+    parser.add_argument("--with-cdc",      action="store_true", help="Enable ClockDomainCrossing.")
     parser.add_argument("--reverse",       action="store_true", help="Reverse converter ordering.")
     parser.add_argument("--build",         action="store_true", help="Build core")
     args = parser.parse_args()
@@ -135,8 +159,14 @@ def main():
         input_width  = input_width,
         output_width = output_width,
         depth        = depth,
+        with_cdc     = args.with_cdc,
+        with_buffer  = args.with_buffer,
         reverse      = args.reverse)
     build_name = "fifo_w{}x{}_r{}".format(input_width, depth, output_width)
+    if args.with_cdc:
+        build_name += "_cdc"
+    if args.with_buffer:
+        build_name += "_buffer"
     if args.build:
         platform.build(module, build_name=build_name, run=False, regular_comb=True)
 
