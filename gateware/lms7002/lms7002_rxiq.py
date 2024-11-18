452773b1969982b2bc93956da1f81b9756015577
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-11-18 07:17:37 +0100

    gateware/lms7002: VHDL -> LiteX lms7002 logic/primitives

---
+++ gateware/lms7002/lms7002_rxiq.py
@@ -0,0 +1,85 @@
+#
+# This file is part of LimeSDR-Mini-v2_GW.
+#
+# Copyright (c) 2024 Lime Microsystems.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from migen import *
+
+from litex.gen import *
+
+from litex.build.io import DDRInput
+
+# LMS7002 RXIQ -------------------------------------------------------------------------------------
+
+class LMS7002RXIQ(LiteXModule):
+    def __init__(self, iq_width=12, pads=None):
+        # Delay control
+        self.data_loadn     = Signal()
+        self.data_move      = Signal()
+        self.data_direction = Signal()
+        self.data_cflag     = Signal()
+        # Output ports to internal logic
+        self.rx_diq2_h      = Signal(iq_width + 1)
+        self.rx_diq2_l      = Signal(iq_width + 1)
+
+        # # #
+
+        # Signals.
+
+        datain               = Signal(iq_width + 1)
+        datain_h             = Signal(iq_width + 1)
+        datain_l             = Signal(iq_width + 1)
+        datain_reg_h         = Signal(iq_width + 1)
+        datain_reg_l         = Signal(iq_width + 1)
+        datain_reg_l_delayed = Signal(iq_width + 1)
+        gen_delay_cflag      = Signal(iq_width + 1)
+
+        for i in range(iq_width + 1):
+            buf_datain     = Signal()
+            delay_datain = Signal()
+
+            self.specials += [
+                # Input buffers.
+                # --------------
+                Instance("IB",
+                    i_I = datain[i],
+                    o_O = buf_datain,
+                ),
+                # Delay components.
+                # -----------------
+                Instance("DELAYF",
+                    p_DEL_VALUE = 1,
+                    p_DEL_MODE  = "USER_DEFINED",
+                    i_A         = buf_datain,
+                    i_LOADN     = self.data_loadn,
+                    i_MOVE      = self.data_move,
+                    i_DIRECTION = self.data_direction,
+                    o_Z         = delay_datain,
+                    o_CFLAG     = gen_delay_cflag[i],
+                ),
+                # IDDR components.
+                # ----------------
+                DDRInput(
+                    clk = ClockSignal("lms_rx"),
+                    i   = delay_datain,
+                    o1  = datain_h[i],
+                    o2  = datain_l[i],
+                ),
+            ]
+
+            # Logic.
+            # ------
+            self.comb += datain.eq(Cat(pads.DIQ2_D, pads.ENABLE_IQSEL2))
+
+            # Internal registers
+            # Resync both on rising edge
+            self.sync.lms_rx += [
+                self.rx_diq2_h.eq(datain_h),
+                datain_reg_l.eq(  datain_l),
+                #- We need to delay data captured on falling edge, in order to allign samples
+                self.rx_diq2_l.eq(     datain_reg_l),
+            ]
+
+            self.comb += self.data_cflag.eq(Reduce("OR", gen_delay_cflag)) # OR all vector bits
---
+++ gateware/lms7002/lms7002_top.py
@@ -0,0 +1,301 @@
+#
+# This file is part of LimeSDR-Mini-v2_GW.
+#
+# Copyright (c) 2024 Lime Microsystems.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from migen import *
+from migen.genlib.cdc       import MultiReg
+from migen.genlib.resetsync import AsyncResetSynchronizer
+
+from litex.gen import *
+
+from litex.soc.interconnect.csr import *
+
+from gateware.lms7002.lms7002_rxiq import LMS7002RXIQ
+
+# UTILS --------------------------------------------------------------------------------------------
+
+class DelayControl(LiteXModule):
+    def __init__(self):
+        self.en    = Signal()
+        self.sel   = Signal(2)
+        self.dir   = Signal()
+        self.mode  = Signal()
+        self.done  = Signal()
+        self.error = Signal()
+
+    def connect(self, slave_delay_control):
+        return [
+            slave_delay_control.en.eq(self.en),
+            slave_delay_control.sel.eq(self.sel),
+            slave_delay_control.dir.eq(self.dir),
+            slave_delay_control.mode.eq(self.mode),
+            self.done.eq(slave_delay_control.done),
+            self.error.eq(slave_delay_control.error),
+        ]
+
+class SampleCompare(LiteXModule):
+    def __init__(self):
+        self.en    = Signal()
+        self.done  = Signal()
+        self.error = Signal()
+        self.cnt   = Signal(16)
+
+    def connect(self, slave_sample_compare):
+        return [
+            slave_sample_compare.en.eq(self.en),
+            self.done.eq(slave_sample_compare.done),
+            self.error.eq(slave_sample_compare.error),
+            slave_sample_compare.cnt.eq(self.cnt),
+        ]
+
+# LMS7002 Top --------------------------------------------------------------------------------------
+
+class LMS7002Top(LiteXModule):
+    def __init__(self, platform, pads=None, hw_ver=None, add_csr=True):
+
+        assert pads   is not None
+        assert hw_ver is not None
+
+        self.pads                = pads
+        self.periph_output_val_1 = Signal(16)
+
+        self.tx_diq1_h = Signal(13)
+        self.tx_diq1_l = Signal(13)
+
+        self.rx_diq2_h = Signal(13)
+        self.rx_diq2_l = Signal(13)
+
+        self.delay_ctrl_en    = Signal()
+        self.delay_ctrl_sel   = Signal(2)
+        self.delay_ctrl_dir   = Signal()
+        self.delay_ctrl_mode  = Signal()
+        self.delay_ctrl_done  = Signal()
+        self.delay_ctrl_error = Signal()
+        self.smpl_cmp         = SampleCompare()
+        self.hw_ver           = Signal(4)
+
+        # # #
+
+        # Clock Domains.
+        # --------------
+        self.cd_lms_rx = ClockDomain()
+        self.cd_lms_tx = ClockDomain()
+
+        # Signals.
+        # --------
+        smpl_cmp_done_sync  = Signal()
+        smpl_cmp_error_sync = Signal()
+
+        delay_sel           = Signal(2)
+
+        inst0_loadn         = Signal()
+        inst0_move          = Signal()
+
+        inst1_delayf_loadn  = Signal()
+        inst1_delayf_move   = Signal()
+
+        tx_data_loadn       = Signal()
+        tx_data_move        = Signal()
+
+        rx_data_loadn       = Signal()
+        rx_data_move        = Signal()
+
+        # CMP Resync.
+        # -----------
+        self.specials += [
+            MultiReg(self.smpl_cmp.error, smpl_cmp_error_sync, "lms_rx"),
+            MultiReg(self.smpl_cmp.done,  smpl_cmp_done_sync,  "lms_rx"),
+        ]
+
+        # Clocks.
+        # -------
+        self.specials += Instance("lms7002_clk",
+            # Clk/Reset.
+            i_mclk1          = pads.MCLK1,
+            i_mclk1_reset_n  = ~ResetSignal("lms_tx"),
+            i_mclk2          = pads.MCLK2,
+            i_mclk2_reset_n  = ~ResetSignal("lms_rx"),
+
+            # Forwarded clock outputs
+            o_fclk1          = pads.FCLK1,
+            o_fclk2          = pads.FCLK2,
+
+            # Clock for internal logic
+            o_mclk1_int      = self.cd_lms_tx.clk,
+            o_mclk2_int      = self.cd_lms_rx.clk,
+
+            # Configuration
+            i_sel            = Constant(0, 1), # 0 - fclk1 control, 1 - fclk2 control
+            o_cflag          = Open(),
+            i_direction      = Constant(0, 1),
+            i_loadn          = inst0_loadn,
+            i_move           = inst0_move
+        )
+
+        # TX Path (DIQ1).
+        # ---------------
+        self.specials += Instance("lms7002_txiq",
+            # Parameters.
+            p_iq_width       = 12,
+
+            # Clk/Reset.
+            i_clk            = ClockSignal("lms_tx"),
+            i_reset_n        = ~ResetSignal("lms_tx"),
+
+            # LMS7002 TX pads.
+            o_txiq           = pads.DIQ1_D,
+            o_txiqsel        = pads.ENABLE_IQSEL1,
+
+            # Delay control
+            i_data_loadn     = tx_data_loadn,
+            i_data_move      = tx_data_move,
+            i_data_direction = Constant(0, 1),
+            o_data_cflag     = Open(),
+
+            # From internal logic
+            i_tx_diq1_h      = self.tx_diq1_h,
+            i_tx_diq1_l      = self.tx_diq1_l
+        )
+
+        # RX path (DIQ2).
+        # ---------------
+        self.lms7002_rxiq = LMS7002RXIQ(12, pads)
+        self.comb += [
+            self.lms7002_rxiq.data_loadn.eq(    rx_data_loadn),
+            self.lms7002_rxiq.data_move.eq(     rx_data_move),
+            self.lms7002_rxiq.data_direction.eq(Constant(0, 1)),
+
+            # Output ports to internal logic.
+            self.rx_diq2_h.eq(self.lms7002_rxiq.rx_diq2_h),
+            self.rx_diq2_l.eq(self.lms7002_rxiq.rx_diq2_l),
+        ]
+
+        # Delay control module.
+        # ---------------------
+        self.specials += Instance("delay_ctrl_top",
+            # Clk/Reset.
+            i_clk              = ClockSignal("lms_rx"),
+            i_reset_n          = ~ResetSignal("lms_rx"),
+
+            #
+            i_delay_en         = self.delay_ctrl_en,
+            i_delay_sel        = self.delay_ctrl_sel,
+            i_delay_dir        = self.delay_ctrl_dir,
+            i_delay_mode       = self.delay_ctrl_mode,
+            o_delay_done       = self.delay_ctrl_done,
+            o_delay_error      = self.delay_ctrl_error,
+
+            # signals from sample compare module (required for automatic phase searching)
+            o_smpl_cmp_en      = self.smpl_cmp.en,
+            i_smpl_cmp_done    = smpl_cmp_done_sync,
+            i_smpl_cmp_error   = smpl_cmp_error_sync,
+            o_smpl_cmp_cnt     = self.smpl_cmp.cnt,
+
+            o_delayf_loadn     = inst1_delayf_loadn,
+            o_delayf_move      = inst1_delayf_move,
+            o_delayf_direction = Open(),
+        )
+
+        # Logic.
+        # ------
+        self.comb += [
+            # lms7002_clk"
+            If(~self.delay_ctrl_sel == 0b00,
+                inst0_loadn.eq(inst1_delayf_loadn),
+                inst0_move.eq (inst1_delayf_move),
+            ).Else(
+                inst0_loadn.eq(1),
+                inst0_move.eq (0),
+            ),
+            # lms7002_tx
+            If(self.delay_ctrl_sel == 0b01,
+                tx_data_loadn.eq(inst1_delayf_loadn),
+                tx_data_move.eq (inst1_delayf_move),
+            ).Else(
+                tx_data_loadn.eq(1),
+                tx_data_move.eq (0),
+            ),
+            # lms7002_rx
+            If(self.delay_ctrl_sel == 0b11,
+                rx_data_loadn.eq(inst1_delayf_loadn),
+                rx_data_move.eq (inst1_delayf_move),
+            ).Else(
+                rx_data_loadn.eq(1),
+                rx_data_move.eq (0),
+            ),
+        ]
+
+        self.specials += AsyncResetSynchronizer(self.cd_lms_rx, ResetSignal("sys"))
+        self.specials += AsyncResetSynchronizer(self.cd_lms_tx, ResetSignal("sys"))
+
+        if add_csr:
+            self.add_csr()
+        self.add_sources(platform)
+
+    def add_csr(self):
+        # LMS Ctrl GPIO
+        self._lms_ctr_gpio = CSRStorage(size=4, description="LMS Control GPIOs.")
+
+        # fpgacfg
+        self.lms1              = CSRStorage(fields=[         # 19
+            CSRField("ss",          size=1, offset=0, reset=1),
+            CSRField("reset",       size=1, offset=1, reset=1),
+            CSRField("core_ldo_en", size=1, offset=2, reset=0),
+            CSRField("txnrx1",      size=1, offset=3, reset=1),
+            CSRField("txnrx2",      size=1, offset=4, reset=0),
+            CSRField("txen",        size=1, offset=5, reset=1),
+            CSRField("rxen",        size=1, offset=6, reset=1),
+        ])
+
+        # pllcfg
+        self.reg01     = CSRStatus(16,  reset=1)
+        self.reg03     = CSRStorage(16, fields=[
+            CSRField("pllcfg_start", size=1, offset=0),
+            CSRField("phcfg_start",  size=1, offset=1),
+            CSRField("pllrst_start", size=1, offset=2),
+            CSRField("pll_ind",      size=5, offset=3),
+            CSRField("cnt_ind",      size=5, offset=8),
+            CSRField("phcfg_updn",   size=1, offset=13),
+            CSRField("phcfg_mode",   size=1, offset=14),
+            CSRField("phcfg_tst",    size=1, offset=15),
+        ], reset=0)
+
+        self.comb += [
+            # LMS Controls.
+            If((self.hw_ver > 5),
+                self.pads.TXNRX2_or_CLK_SEL.eq(self.periph_output_val_1),
+            ).Else(
+                self.pads.TXNRX2_or_CLK_SEL.eq(self.lms1.fields.txnrx2),
+            ),
+            self.pads.TXEN.eq(       self.lms1.fields.txen),
+            self.pads.RXEN.eq(       self.lms1.fields.rxen),
+            self.pads.CORE_LDO_EN.eq(self.lms1.fields.core_ldo_en),
+            self.pads.TXNRX1.eq(     self.lms1.fields.txnrx1),
+            self.pads.RESET.eq(      self.lms1.fields.reset & self._lms_ctr_gpio.storage[0]),
+
+            # pllcfg
+            self.reg01.status.eq(Cat(1, 0, self.delay_ctrl_done, self.delay_ctrl_error, Constant(0, 12))),
+            self.delay_ctrl_en.eq(self.reg03.fields.phcfg_start),
+            If(self.reg03.fields.cnt_ind == 0b0011,
+                self.delay_ctrl_sel.eq(0),
+            ).Else(
+                self.delay_ctrl_sel.eq(3),
+            ),
+            self.delay_ctrl_dir.eq( self.reg03.fields.phcfg_updn),
+            self.delay_ctrl_mode.eq(self.reg03.fields.phcfg_mode),
+
+        ]
+
+    def add_sources(self, platform):
+        lms7002_files = [
+            "gateware/hdl/lms7002/lms7002_clk.vhd",
+            "gateware/hdl/lms7002/lms7002_top.vhd",
+            "gateware/hdl/lms7002/lms7002_rxiq.vhd",
+            "gateware/hdl/lms7002/lms7002_txiq.vhd",
+        ]
+
+        for file in lms7002_files:
+            platform.add_source(file)
