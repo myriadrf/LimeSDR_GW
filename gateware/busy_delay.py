62aab79794fe74e0e1867bcede6f2ad3712b27c3
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-11-28 17:21:17 +0100

    gateware/busy_delay.py: convert to GHDL

--- gateware/busy_delay.py
+++ gateware/busy_delay.py
@@ -5,9 +5,13 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import os
+
 from migen import *
 
 from litex.gen import *
+
+from litex.build.vhd2v_converter import VHD2VConverter
 
 # Busy Delay ---------------------------------------------------------------------------------------
 
@@ -17,6 +21,8 @@ class BusyDelay(LiteXModule):
         delay_time         = 100,
         ):
 
+        self.platform = platform
+
         self.busy_in  = Signal()
         self.busy_out = Signal()
 
@@ -25,7 +31,8 @@ class BusyDelay(LiteXModule):
         # busy_delay instance.
         # -------------------------
 
-        self.specials += Instance("busy_delay",
+        self.busy_delay_params = dict()
+        self.busy_delay_params.update(
             # Parameters
             p_clock_period = clock_period,
             p_delay_time   = delay_time,
@@ -39,7 +46,13 @@ class BusyDelay(LiteXModule):
             o_busy_out     = self.busy_out,
         )
 
-        self.add_sources(platform)
-
-    def add_sources(self, platform):
-        platform.add_source("gateware/hdl/general/busy_delay.vhd")
+    def do_finalize(self):
+        self.busy_delay = VHD2VConverter(self.platform,
+            top_entity    = "busy_delay",
+            build_dir     = os.path.join(os.path.abspath(self.platform.output_dir), "vhd2v"),
+            work_package  = "work",
+            force_convert = LiteXContext.platform.vhd2v_force,
+            params        = self.busy_delay_params,
+            add_instance  = True,
+        )
+        self.busy_delay.add_source("gateware/hdl/general/busy_delay.vhd")
