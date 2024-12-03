746930863aa89195addeebbc9724aaddabdce1a0
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-12-03 17:05:42 +0100

    limesdr_mini_vX: imcrease rom_size when with-bios (not fit with spi-flash enabled)

--- limesdr_mini_v1.py
+++ limesdr_mini_v1.py
@@ -124,7 +124,7 @@ class BaseSoC(SoCCore):
         }[cpu_type]
 
         if with_bios:
-            integrated_rom_size      = 0x6800
+            integrated_rom_size      = 0x9800
             integrated_rom_init      = []
             integrated_main_ram_size = 0x3800
             integrated_main_ram_init = [] if cpu_firmware is None else get_mem_data(cpu_firmware, endianness="little")
