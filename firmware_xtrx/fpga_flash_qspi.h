1806b49297458722ceede17f9a451aace9a65b4b
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-12-04 10:48:54 +0100

    firmware_xtrx: refactor (align code to LimeSDR_GW

--- firmware_xtrx/fpga_flash_qspi.h
+++ firmware_xtrx/fpga_flash_qspi.h
@@ -34,8 +34,8 @@ void Spi_Transfer(uint8_t * sendbuff, uint8_t * recvbuff, unsigned int ByteCount
 //void Init_flash_qspi(u16 DeviceId, XSpi *InstancePtr, u32 Options);
 //int FlashQspi_CMD_DisQPI(XSpi *InstancePtr);
 //int FlashQspi_CMD(XSpi *InstancePtr, u8 cmd);
-int FlashQspi_CMD_ReadRDSR(uint8_t* Data);
-int FlashQspi_CMD_ReadRDCR(uint8_t* Data);
+void FlashQspi_CMD_ReadRDSR(uint8_t* Data);
+void FlashQspi_CMD_ReadRDCR(uint8_t* Data);
 void FlashQspi_CMD_WREN(void);
 void FlashQspi_CMD_WRDI(void);
 //int FlashQspi_CMD_WRSR(XSpi *InstancePtr, u8 StatusReg, u8 ConfigReg);
@@ -43,14 +43,14 @@ void FlashQspi_CMD_WRDI(void);
 //int FlashQspi_CMD_ReadDataPage(XSpi *InstancePtr, u32 address, u8* buffer);
 void FlashQspi_CMD_ReadDataByte(uint32_t address, uint8_t* buffer);
 //int FlashQspi_CMD_ReadOTPData(XSpi *InstancePtr, u32 address, u8 bytes, u8* buffer);
-//int FlashQspi_CMD_WriteDataPage(XSpi *InstancePtr, u32 address, u8* buffer);
+void FlashQspi_CMD_WriteDataPage(uint32_t address, uint8_t* buffer);
 //int FlashQspi_CMD_PageProgram(XSpi *InstancePtr, u32 address, u8 bytes, u8* buffer);
-int FlashQspi_CMD_SectorErase(uint32_t address);
+void FlashQspi_CMD_SectorErase(uint32_t address);
 
-//int FlashQspi_ProgramPage(XSpi *InstancePtr, u32 address, u8* data);
-int FlashQspi_CMD_PageProgramByte(uint32_t address, uint8_t* byte);
+void FlashQspi_ProgramPage(uint32_t address, uint8_t* data);
+void FlashQspi_CMD_PageProgramByte(uint32_t address, uint8_t* byte);
 //int FlashQspi_ProgramOTP(XSpi *InstancePtr, u32 address, u8 bytes, u8* data);
-//int FlashQspi_EraseSector(XSpi *InstancePtr, u32 address);
+void FlashQspi_EraseSector(uint32_t address);
 //int FlashQspi_ReadPage(XSpi *InstancePtr, u32 address, u8* data);
 
 #endif /* SRC_FPGA_FLASH_QSPI_H_ */
--- firmware_xtrx/lms7002m.c
+++ firmware_xtrx/lms7002m.c
@@ -7,28 +7,24 @@
 
 #include "lms7002m.h"
 
-void lms_spi_write(uint16_t addr, uint16_t val)
-{
-	uint16_t cmd;
-	uint16_t dat;
-	cmd = (1 << 15) | (addr & 0x7fff);
-	dat = val & 0xffff;
-	while ((spimaster_status_read() & 0x1) == 0);
-	spimaster_mosi_write(cmd << 16 | dat);
-    spimaster_control_write(32*SPI_LENGTH | SPI_START);
-
-
+void lms_spi_write(uint16_t addr, uint16_t val) {
+    uint16_t cmd;
+    uint16_t dat;
+    cmd = (1 << 15) | (addr & 0x7fff);
+    dat = val & 0xffff;
+    while ((spimaster_status_read() & 0x1) == 0);
+    spimaster_mosi_write(cmd << 16 | dat);
+    spimaster_control_write(32 * SPI_LENGTH | SPI_START);
 }
 
-uint16_t lms_spi_read(uint16_t addr)
-{
-	uint16_t recv_val;
+uint16_t lms_spi_read(uint16_t addr) {
+    uint16_t recv_val;
 
     uint16_t cmd;
     cmd = (0 << 15) | (addr & 0x7fff);
 
     spimaster_mosi_write(cmd << 16);
-    spimaster_control_write(32*SPI_LENGTH | SPI_START);
+    spimaster_control_write(32 * SPI_LENGTH | SPI_START);
     while ((spimaster_status_read() & 0x1) == 0);
     recv_val = spimaster_miso_read() & 0xffff;
     return recv_val;
