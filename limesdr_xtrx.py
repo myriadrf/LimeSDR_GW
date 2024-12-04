8265bc6af7689cf1e57b4f11170e96c7a572f820
Gwenhael Goavec-Merou <gwenhael@enjoy-digital.fr>
2024-12-04 18:24:41 +0100

    limesdr_xtrx.py: adapt/align to LimeSDR_GW target

--- limesdr_xtrx.py
+++ limesdr_xtrx.py
@@ -64,31 +64,38 @@ class CRG(LiteXModule):
     def __init__(self, platform, sys_clk_freq):
         self.cd_sys    = ClockDomain()
         self.cd_idelay = ClockDomain()
+        self.cd_usb = ClockDomain()
+        self.cd_xo_fpga = ClockDomain()
 
         # # #
+        self.clk26 = platform.request("clk26")
+
+        self.comb += self.cd_xo_fpga.clk.eq(self.clk26)
 
         # Clk / Rst.
         clk125 = ClockSignal("pcie")
         rst125 = ResetSignal("pcie")
 
         # PLL.
-        self.pll = pll = S7PLL(speedgrade=-1)
+        self.pll = pll = S7MMCM(speedgrade=-2)
         self.comb += pll.reset.eq(rst125)
         pll.register_clkin(clk125, 125e6)
-        pll.create_clkout(self.cd_sys,    sys_clk_freq)
-        pll.create_clkout(self.cd_idelay, 200e6)
+        pll.create_clkout(self.cd_idelay, 200e6, margin=0)
+        pll.create_clkout(self.cd_sys,    sys_clk_freq, margin=0)
+        pll.create_clkout(self.cd_usb, 26e6, margin=0)
 
         # IDelayCtrl.
         self.idelayctrl = S7IDELAYCTRL(self.cd_idelay)
 
 # LMS Control CSR----------------------------------------------------------------------------------------
 class CNTRL_CSR(LiteXModule):
-    def __init__(self, ndmas):
+    def __init__(self, ndmas, nuart):
         self.cntrl          = CSRStorage(512, 0)
         self.enable         = CSRStorage()
         self.test           = CSRStorage(32)
         self.ndma           = CSRStatus(4, reset=ndmas)
         self.enable_both    = CSRStorage()
+        self.nuart          = CSRStatus(4, reset=nuart)
 
         # Create event manager for interrupt
         self.ev = EventManager()
@@ -98,6 +105,23 @@ class CNTRL_CSR(LiteXModule):
         # Trigger interrupt when cntrl register is written
         self.comb += self.ev.cntrl_isr.trigger.eq(self.cntrl.re)
 
+# periphcfg
+class periphcfg_csr(LiteXModule):
+    def __init__(self):
+        self.BOARD_GPIO_OVRD        = CSRStorage(16, reset=0)
+        self.BOARD_GPIO_RD          = CSRStorage(16, reset=0)
+        self.BOARD_GPIO_DIR         = CSRStorage(16, reset=0)
+        self.BOARD_GPIO_VAL         = CSRStorage(16, reset=0)
+        self.PERIPH_INPUT_SEL_0     = CSRStorage(16, reset=0)
+        self.PERIPH_INPUT_RD_0      = CSRStorage(16, reset=0)
+        self.PERIPH_INPUT_RD_1      = CSRStorage(16, reset=0)
+        self.PERIPH_OUTPUT_OVRD_0   = CSRStorage(16, reset=0)
+        self.PERIPH_OUTPUT_VAL_0    = CSRStorage(16, reset=0)
+        self.PERIPH_OUTPUT_OVRD_1   = CSRStorage(16, reset=0)
+        self.PERIPH_OUTPUT_VAL_1    = CSRStorage(16, reset=0)
+        self.PERIPH_EN              = CSRStorage(16, reset=0)
+        self.PERIPH_SEL             = CSRStorage(16, reset=0)
+
 # BaseSoC -----------------------------------------------------------------------------------------
 
 class BaseSoC(SoCCore):
@@ -105,33 +129,38 @@ class BaseSoC(SoCCore):
         # SoC.
         "uart"        : 0,
         "icap"        : 1,
-        "flash"       : 10, #10
-        "xadc"        : 11, #11
-        "dna"         : 12, #12
 
         # PCIe.
         "pcie_phy"    : 2, #10
         "pcie_msi"    : 3, #11
         "pcie_dma0"   : 5, #12
+        "PCIE_UART0"  : 13,
+        "PCIE_UART1"  : 14,
+
+        "flash"       : 15,  # 10
+        "xadc"        : 16,  # 11
+        "dna"         : 17,  # 12
 
         # XTRX.
-        "i2c0"        : 20,
-        "i2c1"        : 21,
-
-        # Analyzer.
-        "analyzer"    : 30,
+        "i2c0"        : 18,
+        "i2c1"        : 19,
 
         # CNTRL
         "CNTRL"       : 26,
+
+        # Analyzer.
+        "analyzer"    : 31,
+
     }
 
-    def __init__(self, board="limesdr", sys_clk_freq=int(125e6), cpu_type="picorv32",
+    def __init__(self, board="limesdr", sys_clk_freq=int(100e6), cpu_type="picorv32",
         with_bios             = False,
         with_uartbone         = False,
         with_cpu              = True, cpu_firmware=None,
         with_jtagbone         = True,
         with_bscan            = False,
         flash_boot            = False,
+        gold_img              = False,
         firmware_flash_offset = 0x220000,
     ):
 
@@ -142,6 +171,7 @@ class BaseSoC(SoCCore):
             "limesdr"       : limesdr_xtrx_platform.Platform()
         }[board]
 
+        platform.name        = "limesdr_xtrx"
         platform.vhd2v_force = False
 
         # Enable Compressed Instructions.
@@ -189,7 +219,8 @@ class BaseSoC(SoCCore):
         # Avoid stalling CPU at startup.
         self.uart.add_auto_tx_flush(sys_clk_freq=sys_clk_freq, timeout=1, interval=128)
 
-        self.CNTRL = CNTRL_CSR(1)
+        self.periphcfg = periphcfg_csr()
+        self.CNTRL = CNTRL_CSR(1, 2)
         self.irq.add("CNTRL")
 
         # CRG --------------------------------------------------------------------------------------
@@ -206,10 +237,22 @@ class BaseSoC(SoCCore):
             self.add_jtag_cpu_debug()
 
         # Leds -------------------------------------------------------------------------------------
-        self.leds = LedChaser(
-            pads         = platform.request_all("user_led"),
-            sys_clk_freq = sys_clk_freq
-        )
+        # self.led_pads = platform.request_all("user_led")
+        self.led_placeholder = Signal()
+        if gold_img:
+            self.leds = LedChaser(
+                pads         = self.led_placeholder,
+                period       = 2,
+                sys_clk_freq = sys_clk_freq
+            )
+            self.comb += platform.request("user_led",0).eq(self.led_placeholder)
+            self.comb += platform.request("user_led",1).eq(self.led_placeholder)
+        else:
+            self.leds = LedChaser(
+                pads         = platform.request_all("user_led"),
+                period       = 1,
+                sys_clk_freq = sys_clk_freq
+            )
 
         # AXI MMAP ---------------------------------------------------------------------------------
         # AXI MMAP Bus (From CPU).
@@ -218,7 +261,6 @@ class BaseSoC(SoCCore):
         self.comb += self.mmap.connect(self.ram.bus)
         # Connect MMAP interface to SoC.
         self.bus.add_slave(name="lime_top_mmap", slave=self.mmap, region=SoCRegion(origin=0x4000_000, size=0x1000))
-
 
         # ICAP -------------------------------------------------------------------------------------
         self.icap = ICAP()
@@ -263,14 +305,21 @@ class BaseSoC(SoCCore):
             cd          = "sys",
         )
         self.pcie_phy.update_config({
+            #"pcie_id_if"               : "false",  #check this
+            "Bar0_64bit"               : "true",
+            "Bar0_Prefetchable"        : "false",
             "Base_Class_Menu"          : "Wireless_controller",
             "Sub_Class_Interface_Menu" : "RF_controller",
             "Class_Code_Base"          : "0D",
             "Class_Code_Sub"           : "10",
             "Revision_ID"              : "0001",
+            "MSI_64b"                  : "true",
+            "MSIx_Table_BIR"           : "BAR_1:0",
+            "MSIx_PBA_BIR"             : "BAR_1:0"
+
             }
         )
-        self.add_pcie(phy=self.pcie_phy, address_width=32, ndmas=1,
+        self.add_pcie(phy=self.pcie_phy, address_width=64, ndmas=1,
             with_dma_buffering    = True, dma_buffering_depth=8192,
             with_dma_loopback     = False,
             with_dma_synchronizer = False,
@@ -309,7 +358,7 @@ class BaseSoC(SoCCore):
             platform.toolchain.pre_placement_commands.append(f"set_clock_groups -group [get_clocks {{{{*s7pciephy_clkout{i}}}}}] -group [get_clocks       icap_clk] -asynchronous")
 
         # FPGA Cfg ---------------------------------------------------------------------------------
-        self.fpgacfg  = FPGACfg(platform, board_id=27, major_rev=1, compile_rev=18)
+        self.fpgacfg  = FPGACfg(platform, board_id=27, major_rev=2, compile_rev=20)
 
         # PLL Cfg ----------------------------------------------------------------------------------
         #self.pllcfg = PLLCfg()
@@ -354,6 +403,7 @@ class BaseSoC(SoCCore):
             TX_IN_PCT_SIZE         = TX_PCT_SIZE,
             TX_IN_PCT_HDR_SIZE     = TX_IN_PCT_HDR_SIZE,
             TX_IN_PCT_DATA_W       = STRM0_FPGA_RX_RWIDTH,
+            tx_s_clk_domain        = "sys",
 
             # RX parameters
             RX_IQ_WIDTH            = LMS_DIQ_WIDTH,
@@ -390,7 +440,7 @@ class BaseSoC(SoCCore):
             self.lms7002_top.source,
             self.rxtx_top.rx_path.sink,
         )
-        self.comb += self.rxtx_top.rx_path.source.connect(self.pcie_dma0.sink, omit=["keep", "id", "dest", "user"])
+        self.comb += self.rxtx_top.rx_path.source.connect(self.pcie_dma0.sink, keep={"valid", "ready", "last", "data"}),
 
         # FT601 -> TX Path -> LMS7002 Pipeline.
         self.comb += [
@@ -403,7 +453,6 @@ class BaseSoC(SoCCore):
             self.lms7002_top.sink,
         )
 
-        # FIXME: Not connected
         self.comb += self.rxtx_top.tx_path.ext_reset_n.eq(self.pcie_dma0.reader.enable)
 
         # LMS SPI -----------------------------------------------------------------------------------
@@ -412,8 +461,8 @@ class BaseSoC(SoCCore):
         # VCTCXO -----------------------------------------------------------------------------------
 
         vctcxo_pads = platform.request("vctcxo")
-        self.comb += vctcxo_pads.sel.eq(0)
-        self.comb += vctcxo_pads.en.eq(1)
+        self.comb += vctcxo_pads.sel.eq(self.fpgacfg.ext_clk)
+        self.comb += vctcxo_pads.en.eq(self.fpgacfg.tcxo_en)
 
         # RF Switches ------------------------------------------------------------------------------
 
@@ -431,6 +480,52 @@ class BaseSoC(SoCCore):
                                                    | (self.lms7002_top.lms7002_clk.CLK_CTRL.PLLCFG_START.re & self.lms7002_top.lms7002_clk.CLK_CTRL.PLLCFG_START.storage == 1)
                                                    | (self.lms7002_top.lms7002_clk.CLK_CTRL.PLLRST_START.re & self.lms7002_top.lms7002_clk.CLK_CTRL.PLLRST_START.storage == 1) )
         self.irq.add("lime_top")
+
+        # GPS serial connected to LimeUART0
+        from litex.soc.cores.uart import UARTPHY
+        from litex.soc.cores.uart import UART
+
+        gps_pads = platform.request("gps")
+        gnss_uart_pads = self.platform.request("gps_serial", loose=True)
+        gnss_uart_phy = UARTPHY(gnss_uart_pads, clk_freq=self.sys_clk_freq, baudrate=9600)
+        pcie_uart0 = UART(gnss_uart_phy, tx_fifo_depth=16, rx_fifo_depth=16, rx_fifo_rx_we=True)
+        self.add_module(name=f"PCIE_UART0_phy", module=gnss_uart_phy)
+        self.add_module(name="PCIE_UART0", module=pcie_uart0)
+
+
+        # VCTCXO tamer
+        self.pps_internal = Signal()
+
+        synchro_pads = platform.request("synchro")
+        self.comb += [
+            If(self.periphcfg.PERIPH_INPUT_SEL_0.storage[0:1] == 0b01,
+                self.pps_internal.eq(synchro_pads.pps_in)
+            ).Else(
+                self.pps_internal.eq(gps_pads.pps)
+            )
+        ]
+
+        # Define a layout for vctcxo_tamer_pads
+        vctcxo_tamer_layout = [("tune_ref", 1)]  # 1-bit wide signal for tune_ref
+        vctcxo_tamer_pads = Record(vctcxo_tamer_layout)
+        vctcxo_tamer_pads.tune_ref =self.pps_internal
+
+        from gateware.LimeDFB.vctcxo_tamer.src.vctcxo_tamer_top import vctcxo_tamer_top
+        self.vctcxo_tamer = vctcxo_tamer_top(platform=platform, vctcxo_tamer_pads=vctcxo_tamer_pads, clk100_domain="sys", vctcxo_clk_domain="xo_fpga")
+        self.comb += self.vctcxo_tamer.RESET_N.eq(self.crg.pll.locked)
+
+
+        vctcxo_tamer_serial_layout = [("rx", 1),
+                                      ("tx", 1)]  # 1-bit wide signal for tune_ref
+        vctcxo_tamer_serial_pads = Record(vctcxo_tamer_serial_layout)
+        self.comb += vctcxo_tamer_serial_pads.rx.eq(self.vctcxo_tamer.UART_TX)
+        self.comb += self.vctcxo_tamer.UART_RX.eq(vctcxo_tamer_serial_pads.tx)
+
+        pcie_uart1_phy = UARTPHY(vctcxo_tamer_serial_pads, clk_freq=self.sys_clk_freq, baudrate=9600)
+        pcie_uart1 = UART(pcie_uart1_phy, tx_fifo_depth=16, rx_fifo_depth=16, rx_fifo_rx_we=True)
+        self.add_module(name=f"PCIE_UART1_phy", module=pcie_uart1_phy)
+        self.add_module(name="PCIE_UART1", module=pcie_uart1)
+
 
     # JTAG CPU Debug -------------------------------------------------------------------------------
 
@@ -467,6 +562,7 @@ def main():
     parser.add_argument("--driver",                action="store_true",     help="Generate PCIe driver from LitePCIe (override local version).")
     parser.add_argument("--flash-boot",            action="store_true",     help="Write Firmware in Flash instead of RAM.")
     parser.add_argument("--firmware-flash-offset", default=0x220000,        help="Firmware SPI Flash offset.")
+    parser.add_argument("--gold",                  action="store_true",     help="Build/Flash golden image instead of user")
 
     # SoC parameters.
     parser.add_argument("--with-bios",      action="store_true", help="Enable LiteX BIOS.")
@@ -488,6 +584,7 @@ def main():
             with_jtagbone         = not args.with_bscan,
             with_bscan            = args.with_bscan,
             flash_boot            = args.flash_boot,
+            gold_img              = args.gold,
             firmware_flash_offset = args.firmware_flash_offset,
         )
         builder = Builder(soc, csr_csv="csr.csv", bios_console="lite")
@@ -499,12 +596,6 @@ def main():
                 False : "linker_rom.ld",
             }[args.with_bios]
             os.system(f"cd firmware_xtrx && make BUILD_DIR={builder.output_dir} LINKER={linker} clean all")
-
-    # Generate LitePCIe Driver.
-    if args.driver:
-        generate_litepcie_software(soc, "software", use_litepcie_software=args.driver)
-    else:
-        generate_litepcie_software_headers(soc, os.path.join("software", "kernel"))
 
     # Load Bistream.
     if args.load:
