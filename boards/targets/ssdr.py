1ae0089b6437214ea2f89ba455a77f038a43f045
Tomas Šervenikas <t.servenikas@limemicro.com>
2026-04-22 15:05:26 +0300

    Add libc_mode="full" to Builder to prevent default "minimal" value

--- boards/targets/ssdr.py
+++ boards/targets/ssdr.py
@@ -713,7 +713,7 @@ def main():
         if prepare and not args.no_soc_json:
             soc.print_soc_hierarchy_json()
 
-        builder = Builder(soc, csr_csv="csr.csv", bios_console="lite")
+        builder = Builder(soc, csr_csv="csr.csv", bios_console="lite", libc_mode="full")
         builder.build(run=build)
         # Firmware build.
         if prepare:
