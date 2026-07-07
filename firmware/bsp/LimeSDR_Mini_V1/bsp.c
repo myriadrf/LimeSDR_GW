0e1fbd28bc3edee97706af0c4d03ba980d19e4aa
vbuitvydas <v.buitvydas@limemicro.com>
2026-07-07 11:27:17 +0300

    LimeSDR_Mini_V2: firmware added compiliation guard for golden image

--- firmware/bsp/LimeSDR_Mini_V1/bsp.c
+++ firmware/bsp/LimeSDR_Mini_V1/bsp.c
@@ -18,8 +18,10 @@ litei2c_regs I2C0_REGS = {.master_active_addr   = CSR_I2C0_MASTER_ACTIVE_ADDR,
 void bsp_init(void)
 {
     general_periph_board_gpio_OVRD_write(0xFF);
+#ifndef GOLDEN_IMAGE
     csr_write_simple(0x0FFF, clk_ctrl_addrs.phcfg_samples);
     csr_write_simple(0x0002, clk_ctrl_addrs.phcfg_step);
+#endif
     // RESET FIFO once on power-up
     ft601_fifo_control_write(1);
     ft601_fifo_control_write(0);
