eedd1ed2082f879594e5b2b2bda524dd3c5c6ddb
vbuitvydas <v.buitvydas@limemicro.com>
2026-07-13 13:26:40 +0300

    Added pull-down resistor to M2_1PPS_FPGA input

--- boards/platforms/hipersdr_44xx_v2_platform.py
+++ boards/platforms/hipersdr_44xx_v2_platform.py
@@ -297,7 +297,7 @@ _io = [
 
     # Synchro.
     ("synchro", 0,
-        Subsignal("pps_in", Pins("J11")), # M2_1PPS_FPGA
+        Subsignal("pps_in", Pins("J11"), Misc("PULLDOWN=True")), # M2_1PPS_FPGA
         Subsignal("pps_out",Pins("F10")), # M2_1PPS_SYNC_OUT
         IOStandard("LVCMOS18"),
     ),
