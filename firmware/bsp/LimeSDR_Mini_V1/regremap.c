ec629b1b26ddd85c6da4f96c75af2c26f54cb204
Tomas Šervenikas <t.servenikas@limemicro.com>
2026-09-15 15:10:59 +0300

    LimeSDR Mini V1: Add clock configuration race condition prevention

--- firmware/bsp/LimeSDR_Mini_V1/regremap.c
+++ firmware/bsp/LimeSDR_Mini_V1/regremap.c
@@ -313,13 +313,13 @@ bool writeCSR(uint8_t *address, uint8_t *wrdata_array)
         limetop_lms7002_top_lms1_write(value);
         break;
     case 0x23:
-        csr_write_simple(value & 0x1, clk_ctrl_addrs.pllcfg_start);
-        csr_write_simple((value >> 1) & 0x1, clk_ctrl_addrs.phcfg_start);
-        csr_write_simple((value >> 2) & 0x1, clk_ctrl_addrs.pllrst_start);
         csr_write_simple((value >> 3) & 0x1F, clk_ctrl_addrs.pll_ind);
         csr_write_simple((value >> 8) & 0x1F, clk_ctrl_addrs.cnt_ind);
         csr_write_simple((value >> 13) & 0x1, clk_ctrl_addrs.phcfg_updn);
         csr_write_simple((value >> 14) & 0x1, clk_ctrl_addrs.phcfg_mode);
+        csr_write_simple(value & 0x1, clk_ctrl_addrs.pllcfg_start);
+        csr_write_simple((value >> 1) & 0x1, clk_ctrl_addrs.phcfg_start);
+        csr_write_simple((value >> 2) & 0x1, clk_ctrl_addrs.pllrst_start);
         break;
     case 0x24:
         csr_write_simple(value, clk_ctrl_addrs.cnt_phase);
